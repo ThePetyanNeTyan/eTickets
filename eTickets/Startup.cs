90e4550d83b7327949e10ebe6674895d1402974e
Petr <example.gmail.com>
2023-03-11 02:23:05 +0300

    add controllers. add views

--- eTickets/Startup.cs
+++ eTickets/Startup.cs
@@ -57,6 +57,10 @@ namespace eTickets
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+
+            //Seed database
+            AppDbInitializer.Seed(app);
         }
     }
 }
